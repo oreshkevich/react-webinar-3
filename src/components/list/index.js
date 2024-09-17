afdc44713a0d6bbcf69e25f7c2f5e6770a7ccf98
Henadzi Arashkevich <oreshkevich85@mail.ru>
2024-09-17 21:44:50 +0300

    feat add task

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import Item from '../item';
 import './style.css';
 
-function List({ list, onDeleteItem, onSelectItem }) {
+function List({ list, addToOrder }) {
   return (
     <div className="List">
       {list.map(item => (
         <div key={item.code} className="List-item">
-          <Item item={item} onDelete={onDeleteItem} onSelect={onSelectItem} />
+          <Item item={item} addToOrder={addToOrder} />
         </div>
       ))}
     </div>
@@ -21,13 +21,11 @@ List.propTypes = {
       code: PropTypes.number,
     }),
   ).isRequired,
-  onDeleteItem: PropTypes.func,
-  onSelectItem: PropTypes.func,
+  addToOrder: PropTypes.func,
 };
 
 List.defaultProps = {
-  onDeleteItem: () => {},
-  onSelectItem: () => {},
+  addToOrder: () => {},
 };
 
 export default React.memo(List);
